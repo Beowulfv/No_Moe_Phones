1b253574fe353729ea12b6a3a690dd04a9e30f40
Beowulfv <beowolfv@gmail.com>
2017-04-05 01:26:09 -0400

    Add files via upload

---
+++ Remove Phones/addons/CfgExileCustomCode.hpp
@@ -0,0 +1,5 @@
+class CfgExileCustomCode 
+{
+	//Remove Phones
+	ExileServer_system_breaching_network_detonateAllCharges = "overwrites\ExileServer_system_breaching_network_detonateAllCharges.sqf";
+};
